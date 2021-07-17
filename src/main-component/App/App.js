8e93b4df92a9bc7626753fc169a90158a2300d8b
amitnyc83 <amit_nyc@live.com>
2021-07-17 18:38:14 -0400

    first commit

---
+++ src/main-component/App/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import AllRoute from '../router';
+import './App.css';
+
+const App = () => {
+
+
+  return (
+    <div className="App-wrapper">
+       <AllRoute />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/main-component/HomePage/index.js
@@ -0,0 +1,14 @@
+import React, {Fragment} from 'react';
+import Header from '../../components/header';
+
+
+
+const Homepage = () => {
+    return (
+        <Fragment>
+            <Header />
+        </Fragment>
+    )
+}
+
+export default Homepage;
